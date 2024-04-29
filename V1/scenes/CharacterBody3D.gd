1bb1fec3c6ed49607cf36b076ca5e5f016d1bac9
Luis <up201306340@fe.up.pt>
2024-04-29 12:23:07 +0100

    first_implementation

---
+++ V1/scenes/CharacterBody3D.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+var speed = 1.0
+var direction = Vector3()
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	randomize_direction()
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle movement.
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	move_and_slide()
+
+func randomize_direction():
+	direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
