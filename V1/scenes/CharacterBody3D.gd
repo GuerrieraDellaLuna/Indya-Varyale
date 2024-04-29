74b59fb31b716c78020c67f2ec3d672865612175
Luis <up201306340@fe.up.pt>
2024-04-29 15:48:48 +0100

    roaming and turning

--- V1/scenes/CharacterBody3D.gd
+++ V1/scenes/CharacterBody3D.gd
@@ -4,10 +4,12 @@ var speed = 1.0
 var direction = Vector3()
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 0
 
 func _ready():
 	randomize_direction()
+	connect("body_entered", _on_body_entered)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,6 +21,9 @@ func _physics_process(delta):
 	velocity.z = direction.z * speed
 
 	move_and_slide()
+
+func _on_body_entered(body: PhysicsBody3D):
+	randomize_direction()
 
 func randomize_direction():
 	direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
