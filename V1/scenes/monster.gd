2ce897a910a07bd3e4138bf9dbcbfd0bbe0ebffc
Luis <up201306340@fe.up.pt>
2024-05-09 17:03:40 +0100

    turning

--- V1/scenes/monster.gd
+++ V1/scenes/monster.gd
@@ -1,13 +1,8 @@
 extends CharacterBody3D
 
-#TODO: UNCOMMENT GRAVITY AND DEAL WITH COLLISIONS WHEN FLOOR AND WALLS PROPERLY IMPLEMENTED
-
 var speed = 1.0
 var direction = Vector3()
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gravity = 0
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	randomize_direction()
@@ -20,10 +15,16 @@ func _physics_process(delta):
 	# Handle movement.
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
+
+	# Rotate the character to face the movement direction.
+	if direction.length_squared() > 0.001:  # Only rotate if there's a significant direction change.
+		var target_rotation = direction.angle_to(Vector3.FORWARD)  # Calculate angle to face direction.
+		rotation.y = target_rotation  # Set the character's y rotation.
+
 	if is_on_wall():
 		randomize_direction()
+	
 	move_and_slide()
-
 
 func randomize_direction():
 	direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
