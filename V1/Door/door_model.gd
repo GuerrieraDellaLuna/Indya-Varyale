d2aa71a276be0e61a4b57ad185457d9409db6d18
GuerrieraDellaLuna <119616116+GuerrieraDellaLuna@users.noreply.github.com>
2024-05-27 15:04:06 +0100

    teleport working

--- V1/Door/door_model.gd
+++ V1/Door/door_model.gd
@@ -3,9 +3,6 @@ extends Node3D
 var player_infront = false
 var door_state = false
 
-@onready var player_scene = preload("res://scenes/player.tscn")
-@onready var spawn_pos_scene_path = $"SpawnPoint"
-
 func _input(event):
 	if Input.is_action_just_pressed("open_door"):
 		if player_infront and not $AnimationPlayer.is_playing():
@@ -24,13 +21,9 @@ func _on_Area_body_exited(body):
 	if body.is_in_group("player"):
 		player_infront = false
 		$Label.hide()
-		if not door_state:
-			$AnimationPlayer.play("DoorClose")
+		$AnimationPlayer.play("DoorClose")
 
 func _on_portal_body_entered(body):
 	if body.is_in_group("player"):
-		var player_instance = body  # Assuming the player is already an instance
-		player_instance.position = Vector3(-9.876,0,-71.484)
-		
 		get_tree().change_scene_to_file("res://scenes/locker_room.tscn")
 		queue_free()
